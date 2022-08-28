b8187aec6cc854a2d45425fc03c70456c475df30
marcobarbosa <marco.barbosa@funasa.gov.br>
2022-08-28 19:26:55 -0300

    Commit inicial

---
+++ src/main/java/com/marco/votacaopauta/VotacaoPautaApplication.java
@@ -0,0 +1,13 @@
+package com.marco.votacaopauta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoPautaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotacaoPautaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marco/votacaopauta/VotacaoPautaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.votacaopauta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoPautaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
