fff5a1feb0c46374e52e998ae84968d9a99eb2ba
marcobarbosa <marco.barbosa@funasa.gov.br>
2022-08-30 01:33:49 -0300

    Adiciona Profiles e banco mysql

--- src/main/java/com/marco/votacaopauta/controller/exception/ResourceExceptionHandler.java
+++ src/main/java/com/marco/votacaopauta/controller/exception/ResourceExceptionHandler.java
@@ -9,13 +9,16 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<StandardError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
         StandardError standardError = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
--- src/main/java/com/marco/votacaopauta/service/dto/PautaDTO.java
+++ src/main/java/com/marco/votacaopauta/service/dto/PautaDTO.java
@@ -3,6 +3,7 @@ package com.marco.votacaopauta.service.dto;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
